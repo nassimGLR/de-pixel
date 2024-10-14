6725c5e92b113878d8eb59fa992a103598163600
MichaelD1B <102457@glr.nl>
2024-10-14 11:56:33 +0000

    test

---
+++ websites/michael/js/script.js
@@ -0,0 +1,4 @@
+        // JavaScript function to navigate to the next page on key press
+        document.addEventListener('keydown', function() {
+            window.location.href = '/websites/michael/pages/page2.html';
+        });
